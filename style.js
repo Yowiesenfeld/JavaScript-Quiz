ccb27b9ef5eaa505b8e6d08ed6c083c879d7ff11
Yowiesenfeld <136192424+Yowiesenfeld@users.noreply.github.com>
2023-07-27 14:22:10 -0400

    i added some javascript and i did somemore css

--- style.js
+++ style.js
@@ -1,1 +1,36 @@
-  
+ const questions = [
+    {
+        question: "What does a cow say?",
+        answers: [
+            { text: "moo", correct: true },
+            { text: "baa", correct: false },
+            { text: "haha", correct: false },
+            { text: "roar", correct: false },
+        ]
+    },
+    {
+        question: "What does a sheep say?",
+        answers: [
+            { text: "moo", correct: false },
+            { text: "baa", correct: true },
+            { text: "haha", correct: false },
+            { text: "roar", correct: false }, 
+    },
+    {
+        question: "What does a lion say?",
+        answers: [
+            { text: "moo", correct: false },
+            { text: "baa", correct: false },
+            { text: "haha", correct: false },
+            { text: "roar", correct: true },
+    },
+    {
+        question: "what does a horse say?",
+        answers: [
+            { text: "moo", correct: true },
+            { text: "baa", correct: false },
+            { text: "roar", correct: false },
+            { text: "none of the above", correct: true },
+    }
+
+ ]
