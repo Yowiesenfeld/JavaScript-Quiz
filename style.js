ac6158f1a911edfec0737e15af8a888a10e3eb20
Yowiesenfeld <136192424+Yowiesenfeld@users.noreply.github.com>
2023-07-27 16:37:50 -0400

    still working on it to make it functional

--- style.js
+++ style.js
@@ -33,4 +33,33 @@
             { text: "none of the above", correct: true },
     }
 
- ]
+ ];
+
+ const questionElement = document.getElementById("question");
+ const answerButton = document.getElementById("answer-buttons");
+ const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function,startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionElementNo = currentQuestionIndex + 1;
+    questionElement.innerHTML =questionNo + "."+ currentQuestion.
+    question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
+
